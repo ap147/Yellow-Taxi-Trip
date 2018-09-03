89a830c92d3fdec5121fe3ed309e3dc45b2faaa7
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-09-04 10:44:17 +1200

    lambda function - getDriverDetails (returns 100 drivers)

---
+++ lambdaFunctions/getDriverDetails.js
@@ -0,0 +1,20 @@
+  'use strict';
+
+  const AWS = require('aws-sdk');
+  const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2' });
+
+  exports.handler = function(e, ctx, callback) {
+
+      let scanningParameters = {
+          TableName: 'drivers',
+          Limit: 100
+      };
+
+      docClient.scan(scanningParameters, function(err, data) {
+          if (err) {
+              callback(err, null);
+          } else {
+              callback(null, data);
+          }
+      });
+  }
