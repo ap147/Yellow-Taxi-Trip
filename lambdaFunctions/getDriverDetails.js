78f4ee08f662f75ff65d6ef1cbbdf58d745a4dcc
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-09-04 17:00:46 +1200

    driver pulled by using get (hardcoded id)

--- lambdaFunctions/getDriverDetails.js
+++ lambdaFunctions/getDriverDetails.js
@@ -1,20 +1,43 @@
-  'use strict';
+'use strict';
 
-  const AWS = require('aws-sdk');
-  const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2' });
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2' });
 
-  exports.handler = function(e, ctx, callback) {
+exports.handler = function(e, ctx, callback) {
 
-      let scanningParameters = {
-          TableName: 'drivers',
-          Limit: 100
-      };
+    let parameters = {
+        TableName: 'drivers',
+        Key: {
+            "id": "2"
+        }
+    };
 
-      docClient.scan(scanningParameters, function(err, data) {
-          if (err) {
-              callback(err, null);
-          } else {
-              callback(null, data);
-          }
-      });
-  }
+    // Define the object that will hold the data values returned
+    var response = {
+        statusCode: 200,
+        body: ''
+    };
+
+    docClient.get(parameters, function(err, data) {
+        if (err) {
+            callback(err, null);
+        } else {
+            response["body"] = JSON.stringify(data);
+            callback(null, response);
+        }
+    });
+}
+
+
+// To SCAN REPLACE CODE ABOVE WITH
+// let scanningParameters = {
+// TableName: 'drivers',
+// Limit: 100
+// };
+// docClient.scan(parameters, function(err, data) {
+//     if (err) {
+//         callback(err, null);
+//     } else {
+//         callback(null, data);
+//     }
+// });
