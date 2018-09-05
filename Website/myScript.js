db5919cfccc484a3ff204e432001616343fbd59c
Kamal <kamal.m@live.com>
2018-09-05 15:49:17 +1200

    able to pull data from api - cant extract the data yet

--- Website/myScript.js
+++ Website/myScript.js
@@ -11,8 +11,12 @@ function changePage() {
     else if (document.getElementById("radioDriver").checked == true)
         {
              
-            document.getElementById("login").style.display = "none";
+        document.getElementById("login").style.display = "none";
         document.getElementById("driverpage").style.display = "block";
+        var userid;
+            userid = document.getElementById("usernameID").value;
+            console.log(userid);
+        loadDoc(userid);
         }
     
 }
@@ -25,6 +29,7 @@ function functionViewDrivers() {
     
     document.getElementById("viewTrips").style.display = "none";
     document.getElementById("viewDrivers").style.display = "block";
+    
 
 }
 
@@ -32,4 +37,19 @@ function functionViewTrips() {
     document.getElementById("viewDrivers").style.display = "none";
     document.getElementById("viewTrips").style.display = "block";
 
+}
+
+function loadDoc(loggedID) {
+  
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     var myJSON = JSON.parse(this.responseText);
+        console.log(myJSON);
+    
+        document.getElementById("driverName").innerHTML = myJSON["name"];
+    }
+  };
+  xhttp.open("GET", "https://q6c3ujfk81.execute-api.us-east-2.amazonaws.com/default/getDriverDetails?id=" + loggedID , true);
+  xhttp.send();
 }
