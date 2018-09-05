c27fdeaee583857053867bfd1460f0597c6017db
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-09-05 16:28:58 +1200

    Sets drivers name according to ID

--- Website/myScript.js
+++ Website/myScript.js
@@ -1,35 +1,32 @@
 function changePage() {
-   
+
     console.log("pressed");
-    if (document.getElementById("radioAdmin").checked == true)
-        {
-        
-         document.getElementById("adminpage").style.display = "block";
+    if (document.getElementById("radioAdmin").checked == true) {
+
+        document.getElementById("adminpage").style.display = "block";
         document.getElementById("login").style.display = "none";
-        
-        }
-    else if (document.getElementById("radioDriver").checked == true)
-        {
-             
+
+    } else if (document.getElementById("radioDriver").checked == true) {
+
         document.getElementById("login").style.display = "none";
         document.getElementById("driverpage").style.display = "block";
         var userid;
-            userid = document.getElementById("usernameID").value;
-            console.log(userid);
+        userid = document.getElementById("usernameID").value;
+        console.log(userid);
         loadDoc(userid);
-        }
-    
+    }
+
 }
 
 function backHome() {
-     location.reload();
+    location.reload();
 }
 
 function functionViewDrivers() {
-    
+
     document.getElementById("viewTrips").style.display = "none";
     document.getElementById("viewDrivers").style.display = "block";
-    
+
 
 }
 
@@ -40,16 +37,15 @@ function functionViewTrips() {
 }
 
 function loadDoc(loggedID) {
-  
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     var myJSON = JSON.parse(this.responseText);
-        console.log(myJSON);
-    
-        document.getElementById("driverName").innerHTML = myJSON["name"];
-    }
-  };
-  xhttp.open("GET", "https://q6c3ujfk81.execute-api.us-east-2.amazonaws.com/default/getDriverDetails?id=" + loggedID , true);
-  xhttp.send();
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText).body.Item;
+            console.log(response);
+            document.getElementById("driverName").innerHTML = response.Name;
+        }
+    };
+    xhttp.open("GET", "https://q6c3ujfk81.execute-api.us-east-2.amazonaws.com/default/getDriverDetails?id=" + loggedID, true);
+    xhttp.send();
 }
